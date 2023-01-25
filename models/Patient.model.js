1e4b0ced2ae75eae902a798a6562a1022396178e
Salman <salmanajani0426@gmail.com>
2023-01-25 10:51:09 +0530

    updated patient schema

--- models/Patient.model.js
+++ models/Patient.model.js
@@ -53,22 +53,7 @@ const patientSchema = mongoose.Schema({
   },
 
   address: {
-    street: {
-      type: String,
-      required: true,
-    },
-    city: {
-      type: String,
-      required: true,
-    },
-    state: {
-      type: String,
-      required: true,
-    },
-    pincode: {
-      type: Number,
-      required: true,
-    },
+    type: String,
   },
 
   image: {
