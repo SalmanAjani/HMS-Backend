c3d691463ecd6c68879ef165c9cb2f848e1219e3
piyush-agrawal6 <agrawaljoy1@gmail.com>
2023-01-26 22:47:02 +0530

    updated

--- routes/Hospitals.Route.js
+++ routes/Hospitals.Route.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const { AdminModel } = require("../models/Admin.model");
+const { AmbulanceModel } = require("../models/Ambulance.model");
+const { AppointmentModel } = require("../models/Appointment.model");
+const { BedModel } = require("../models/Bed.model");
+const { DoctorModel } = require("../models/Doctor.model");
+const { NurseModel } = require("../models/Nurse.model");
 const { PatientModel } = require("../models/Patient.model");
+const { ReportModel } = require("../models/Report.model");
 
 const router = express.Router();
 
@@ -8,7 +14,22 @@ router.get("/", async (req, res) => {
   try {
     let admins = await AdminModel.find();
     let patients = await PatientModel.find();
-    let data = { admin: admins.length, patient: patients.length };
+    let ambulances = await AmbulanceModel.find();
+    let nurses = await NurseModel.find();
+    let beds = await BedModel.find();
+    let reports = await ReportModel.find();
+    let appointments = await AppointmentModel.find();
+    let doctors = await DoctorModel.find();
+    let data = {
+      admin: admins.length,
+      patient: patients.length,
+      ambulance: patients.length,
+      nurse: patients.length,
+      bed: beds.length,
+      report: reports.length,
+      doctor: doctors.length,
+      appointment: appointments.length,
+    };
     res.status(200).send({ data });
   } catch (error) {
     console.log(error);
