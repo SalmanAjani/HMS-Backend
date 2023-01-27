d8a012422b398206a0956a0b13d365c8baf6c562
Salman <salmanajani0426@gmail.com>
2023-01-27 11:18:20 +0530

    updated admin route

--- routes/Admins.Route.js
+++ routes/Admins.Route.js
@@ -83,7 +83,7 @@ router.post("/forgotPassword", (req, res) => {
     service: "gmail",
     auth: {
       admin: "salmanajani26@gmail.com",
-      pass: process.env.gmailPass,
+      pass: "Alfa@0426",
     },
   });
 
