99ce17a018736fcfaeab3636f2a6c2501ef53e42
Salman <salmanajani0426@gmail.com>
2023-01-27 11:15:00 +0530

    updated admin route

--- routes/Admins.Route.js
+++ routes/Admins.Route.js
@@ -96,8 +96,7 @@ router.post("/forgotPassword", (req, res) => {
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.log(error);
-      res.status(500).send("Error sending email");
+      res.send(error);
     } else {
       console.log("Email sent: " + info.response);
       res.status(200).send("Password reset email sent");
